0c69abe1e206cc939398ab460925354ec05445cc
AndreaRomano02 <andy.romano2002@gmail.com>
2023-06-06 14:32:03 +0200

    Complete exercise Palindrom Function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log('JS OK');
+console.log("JS OK");
+
+//# Chiedo all'utente una parola
+const userWord = prompt(
+  "Scrivimi una parola e ti controllo se è Palindroma"
+).toLowerCase();
+
+//# Flag
+let isPalindrom = false;
+
+//# Creo la funzione
+function palindromWord(word) {
+  let wordReverse = word.split("").reverse();
+  wordReverse = wordReverse.join("");
+  if (wordReverse === word) isPalindrom = true;
+}
+
+//# Invoco la funzione
+palindromWord(userWord);
+
+//# Controllo il parametro di isPalindrom e dichiaro se è palindroma o no
+if (isPalindrom) console.log("è palindroma");
+else console.log("Non è palindroma");
