affa021dbe5b32e17e1a390cef744905a03d7f45
AndreaRomano02 <andy.romano2002@gmail.com>
2023-06-06 16:27:14 +0200

    Complete ALL

--- js/script.js
+++ js/script.js
@@ -3,35 +3,35 @@ console.log("JS OK");
 // -----------------------------------------------------
 //! PALINDROMI
 
-// //# Recupero gli elementi dal DOM
-// const userInputElement = document.getElementById("user-word");
-// const button = document.getElementById("send");
-// const resultElement = document.getElementById("result");
+//# Recupero gli elementi dal DOM
+const userInputElement = document.getElementById("user-word");
+const button = document.getElementById("send");
+const resultElement = document.getElementById("result");
 
-// //# Flag
-// let isPalindrom;
+//# Flag
+let isPalindrom;
 
-// //# Creo la funzione
-// function palindromWord(word) {
-//   let wordReverse = word.split("").reverse();
-//   wordReverse = wordReverse.join("");
-//   if (wordReverse === word) isPalindrom = true;
-//   else isPalindrom = false;
-// }
+//# Creo la funzione
+function palindromWord(word) {
+  let wordReverse = word.split("").reverse();
+  wordReverse = wordReverse.join("");
+  if (wordReverse === word) isPalindrom = true;
+  else isPalindrom = false;
+}
 
-// //# Attendo il click del bottone
-// button.addEventListener("click", function () {
-//   //# Recupero la parola data dall'utente
-//   const userWord = userInputElement.value.trim().toLowerCase();
+//# Attendo il click del bottone
+button.addEventListener("click", function () {
+  //# Recupero la parola data dall'utente
+  const userWord = userInputElement.value.trim().toLowerCase();
 
-//   //# Invoco la funzione
-//   palindromWord(userWord);
+  //# Invoco la funzione
+  palindromWord(userWord);
 
-//   //# Controllo il parametro di isPalindrom e dichiaro se è palindroma o no
-//   resultElement.innerText = "";
-//   if (isPalindrom) resultElement.innerText = "E' palindroma";
-//   else resultElement.innerText = "Non è palindroma";
-// });
+  //# Controllo il parametro di isPalindrom e dichiaro se è palindroma o no
+  resultElement.innerText = "";
+  if (isPalindrom) resultElement.innerText = "E' palindroma";
+  else resultElement.innerText = "Non è palindroma";
+});
 
 // -----------------------------------------------------
 
